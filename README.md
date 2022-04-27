6651353a216b318f611cb5e74c4206f8c3aae632
adbenitez <asieldbenitez@gmail.com>
2022-04-26 23:04:47 -0400

    set up heroku layout

---
+++ README.md
@@ -0,0 +1,9 @@
+Simplebot WWW
+=============
+
+A [Heroku](https://heroku.com) deployment of simplebot with some selection of plugins to setup a web browsing & file downloader/uploader bot for all your Delta Chat needs :)
+
+To setup your own bot instance just click the button below:
+
+.. image:: https://www.herokucdn.com/deploy/button.svg
+   :target: https://heroku.com/deploy?template=https://github.com/adbenitez/simplebot-www-heroku/tree/main/
---
+++ heroku.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# configure the bot
+python3 -m simplebot init "$ADDR" "$PASSWORD"
+python3 -m simplebot -a "$ADDR" set_name "www"
+python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/mode" "command"
+
+# add the web_comress plugin
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_compress.py'); open('web_compress.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./web_compress.py
+
+# add the web_search plugin
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_search.py'); open('web_search.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./web_search.py
+
+# add the youtube plugin
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/youtube.py'); open('youtube.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./youtube.py
+
+# add the encryption_error plugin to leverage key changes
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/encryption_error.py'); open('encryption_error.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./encryption_error.py
+
+# add admin plugin
+if [ -n "$ADMIN" ]; then
+    python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/admin.py'); open('admin.py', 'wb').write(r.content)"
+    python3 -m simplebot -a "$ADDR" plugin --add ./admin.py
+    python3 -m simplebot -a "$ADDR" admin --add "$ADMIN"
+fi
+
+# start the bot
+python3 -m simplebot -a "$ADDR" serve
